2f22ba4e55c58e196ba0a7579a8ce94bae4ae3ad
Travis Arnold <ftntravis@gmail.com>
2021-01-24 10:49:54 -0800

    fix rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import { Suspense } from 'react'
 import useStore from '@/helpers/store'
 import dynamic from 'next/dynamic'
-import Go from '@/components/dom/go'
 
-const Guitar = dynamic(() => import('@/components/canvas/Guitar'), {
+const Model = dynamic(() => import('@/components/canvas/Guitar'), {
   ssr: false,
 })
 
+const Guitar = () => {
+  return (
+    <Suspense fallback={null}>
+      <ambientLight />
+      <Model />
+    </Suspense>
+  )
+}
 const Page = () => {
   useStore.setState({ title: 'Sphere' })
   return (
